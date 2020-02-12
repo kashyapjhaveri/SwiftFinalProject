f17df6fd1300cc32a5e00e6959089ccff7f19f1b
GagandeepKaur97 <sherry2312.gk@gmail.com>
2020-02-12 00:28:12 -0500

    [person class created]

---
+++ SwiftFinalProject/EnumGender.swift
@@ -0,0 +1,13 @@
+//
+//  EnumGender.swift
+//  SwiftFinalProject
+//
+//  Created by Evneet kaur on 2020-02-11.
+//  Copyright © 2020 Kashyap Jhaveri. All rights reserved.
+//
+
+import Foundation
+
+enum Gender {
+    case Male , Female , Others
+}
--- SwiftFinalProject/Person.swift
+++ SwiftFinalProject/Person.swift
@@ -2,10 +2,40 @@
 //  Person.swift
 //  SwiftFinalProject
 //
-//  Created by Kashyap Jhaveri on 2020-02-11.
+//  Created by Evneet kaur on 2020-02-11.
 //  Copyright © 2020 Kashyap Jhaveri. All rights reserved.
 //
 
 import Foundation
 
-//Person class swift file
+class Person : IDisplay
+{
+   
+    
+    var id : String
+    var  firstname : String
+    var lastname : String
+    var gender : Gender
+    var birthDate : Date
+    var age : Int
+    var mobileNumber : String
+    var email : String
+    var username : String
+    var  password : String
+
+   var  calendar = Calendar.current
+   // func calendar;.component(.year, from: date)
+   // calendar.component(.month, from: date)
+  //  calendar.component(.day, from: date)
+
+    
+    final  var key : UInt8 = 5
+    func Person (){
+        
+        
+    }
+    
+    
+    
+    
+}
