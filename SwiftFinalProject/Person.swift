36cf0f4b27f7b89f38fc62c8005289badc29832a
Kashyap Jhaveri, Shreekant Marella, Gagandeep kaur <jhaverikashyap1998@gmail.com>
2020-02-12 16:36:24 -0500

    [Kashyap Jhaveri] https://shreemarella.atlassian.net/browse/SFP-5 see comments in this task.

--- SwiftFinalProject/Person.swift
+++ SwiftFinalProject/Person.swift
@@ -11,62 +11,44 @@ import Foundation
 class Person : IDisplay
 
 {
-    func Display() {
-        <#code#>
-    }
-    
-   
-    
-    var id : String = ""
-    var  firstname : String = ""
-    var lastname : String = ""
+    var id : String
+    var firstname : String
+    var lastname : String
     var gender : Gender
     var birthDate : Date
-    var age : Int = 0
-    var mobileNumber : String = ""
-    var email : String = ""
-    var username : String = ""
-    var  password : String
+    var age : Int
+    var mobileNumber : String
+    var email : String
+    var username : String
+    var password : String
 
-    
     final  var key : UInt8 = 5
     
-    func Person ()
-        
+    init (id: String, firstname: String, lastname: String, gender: Gender, birthDate: Date, age: Int, mobileNumber: String, email: String, username: String, password: String )
     {
+           self.id = id
+           self.firstname = firstname
+           self.lastname = lastname
+           self.gender = gender
+           self.birthDate = birthDate
+           self.age = age
+           self.mobileNumber = mobileNumber
+           self.email = email
+           self.username = username
+           self.password = password
     }
-        init (id: String, firstname: String, lastname: String, gender: Gender, birthDate: Date, age: Int, mobileNumber: String, email: String, username: String, password: String )
-        {
-               self.id = id
-               self.firstname = firstname
-               self.lastname = lastname
-               self.gender = gender
-               self.birthDate = birthDate
-               self.age = age
-               self.mobileNumber = mobileNumber
-               self.email = email
-               self.username = username
-               self.password = password
-            
-        func Display() {
-            print (" id : \(self.id)")
-            print (" firstname: \(self.firstname)")
-            print (" lastname: \(self.lastname)")
-            print (" gender: \(self.gender)")
-            print (" birthDate: \(self.birthDate)")
-            print (" age: \(self.age)")
-            print (" mobileNumber: \(self.mobileNumber)")
-            print (" email : \(self.email)")
-            print (" username : \(self.username)")
-            print (" password: \(self.password)")
-        }
-
         
-        
+    func Display() {
+        print (" id : \(self.id)")
+        print (" firstname: \(self.firstname)")
+        print (" lastname: \(self.lastname)")
+        print (" gender: \(self.gender)")
+        print (" birthDate: \(self.birthDate)")
+        print (" age: \(self.age)")
+        print (" mobileNumber: \(self.mobileNumber)")
+        print (" email : \(self.email)")
+        print (" username : \(self.username)")
+        print (" password: \(self.password)")
     }
-    
-    
-    
-    
 }
 
