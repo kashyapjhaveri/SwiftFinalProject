c5d9321246ebdf4bcd095edc07f6026ce52b9fc1
GagandeepKaur97 <sherry2312.gk@gmail.com>
2020-02-12 00:48:23 -0500

    [customer class created]

---
+++ SwiftFinalProject/customer.swift
@@ -0,0 +1,14 @@
+//
+//  customer.swift
+//  SwiftFinalProject
+//
+//  Created by Evneet kaur on 2020-02-12.
+//  Copyright © 2020 Kashyap Jhaveri. All rights reserved.
+//
+
+import Foundation
+
+class customer: Person {
+    var address : String
+    var city : String
+}
