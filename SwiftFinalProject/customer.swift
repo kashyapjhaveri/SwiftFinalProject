afd376c039b38682ff4cbc0406b1f77aee158a05
GagandeepKaur97 <sherry2312.gk@gmail.com>
2020-02-12 00:52:46 -0500

    ..

--- SwiftFinalProject/customer.swift
+++ SwiftFinalProject/customer.swift
@@ -2,13 +2,18 @@
 //  customer.swift
 //  SwiftFinalProject
 //
-//  Created by Evneet kaur on 2020-02-12.
+//  Created by gagandeep kaur on 2020-02-12.
 //  Copyright © 2020 Kashyap Jhaveri. All rights reserved.
 //
 
 import Foundation
 
 class customer: Person {
+
+    
     var address : String
     var city : String
+    
+    init (id: String, firstname: String, lastname: String, gender: Gender, birthDate: Date, age: Int, mobileNumber: String, email: String, username: String, password: String )
+
 }
