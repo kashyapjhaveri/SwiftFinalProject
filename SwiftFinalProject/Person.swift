f5419d1349d55a869ceb5de4f540630e1dc25b35
GagandeepKaur97 <sherry2312.gk@gmail.com>
2020-02-12 00:35:47 -0500

    [initializers declared]

--- SwiftFinalProject/Person.swift
+++ SwiftFinalProject/Person.swift
@@ -2,35 +2,52 @@
 //  Person.swift
 //  SwiftFinalProject
 //
-//  Created by Evneet kaur on 2020-02-11.
+//  Created by Gagandeep kaur on 2020-02-11.
 //  Copyright © 2020 Kashyap Jhaveri. All rights reserved.
 //
 
 import Foundation
 
 class Person : IDisplay
+
 {
    
     
-    var id : String
-    var  firstname : String
-    var lastname : String
+    var id : String = ""
+    var  firstname : String = ""
+    var lastname : String = ""
     var gender : Gender
     var birthDate : Date
-    var age : Int
-    var mobileNumber : String
-    var email : String
-    var username : String
+    var age : Int = 0
+    var mobileNumber : String = ""
+    var email : String = ""
+    var username : String = ""
     var  password : String
-
-   var  calendar = Calendar.current
-   // func calendar;.component(.year, from: date)
-   // calendar.component(.month, from: date)
-  //  calendar.component(.day, from: date)
 
     
     final  var key : UInt8 = 5
-    func Person (){
+    
+    func Person ()
+        
+    {
+    }
+        init (id: String, firstname: String, lastname: String, gender: Gender, birthDate: Date, age: Int, mobileNumber: String, email: String, username: String, password: String )
+        {
+               self.id = id
+               self.firstname = firstname
+               self.lastname = lastname
+               self.gender = gender
+               self.birthDate = birthDate
+               self.age = age
+               self.mobileNumber = mobileNumber
+               self.email = email
+               self.username = username
+               self.password = password
+            
+        func Display() {
+            <#code#>
+        }
+
         
         
     }
@@ -39,3 +56,4 @@ class Person : IDisplay
     
     
 }
+
