759d699d23d52b2631dbba43e4e9af71cf2b4dda
shreemarella <mvsrikanth96@gmail.com>
2020-02-21 12:03:04 -0500

    [Shree Marella] created protocol of vehicle

--- SwiftFinalProject/Vehicle/Vehicle.swift
+++ SwiftFinalProject/Vehicle/Vehicle.swift
@@ -13,47 +13,21 @@ enum RatePerKm : Double
     case CAR=7.0, MOTORCYCLE=5.0, BUS=11.0
 }
 
-class Vehicle : IDisplay
+protocol Vehicle : IDisplay
 {
     
-var vehicleIdentificationNumber : String
-var vehicleDescription : String
-var vehicleManufacturerName : String
-var isSelfDrive : Bool
-var isInsured : Bool
-var insuranceProviderName : String
-var noOfSeat : Int
-var fuelType : Fuel
-var baseRate : Int
-var ratePerKm : Int
-var vehicleType : String
-
-init(_ vehicleIdentificationNumber:String,_ vehicleDescription:String,_ vehicleManufacturerName:String,_ isSelfDrive:Bool,_ isInsured:Bool,_ insuranceProviderName:String,_ noOfSeat:Int,_ fuelType:Fuel,_ baseRate:Int,_ ratePerKm:Int,_ vehicleType:String)
-{
-    self.vehicleIdentificationNumber = vehicleIdentificationNumber
-    self.vehicleDescription = vehicleDescription
-    self.vehicleManufacturerName = vehicleManufacturerName
-    self.isSelfDrive = isSelfDrive
-    self.isInsured = isInsured
-    self.insuranceProviderName = insuranceProviderName
-    self.noOfSeat = noOfSeat
-    self.fuelType = fuelType
-    self.baseRate = baseRate
-    self.ratePerKm = ratePerKm
-    self.vehicleType = vehicleType
-}
+var vehicleIdentificationNumber : String {get set}
+var vehicleDescription : String {get set}
+var vehicleManufacturerName : String {get set}
+var isSelfDrive : Bool {get set}
+var isInsured : Bool {get set}
+var insuranceProviderName : String {get set}
+var noOfSeat : Int {get set}
+var fuelType : Fuel {get set}
+var baseRate : Int {get set}
+var ratePerKm : Int {get}
+var vehicleType : String {get set}
+    
 func Display()
-{
-    print("vehicleIdentificationNumber : \(self.vehicleIdentificationNumber)")
-    print("vehicleDescription          : \(self.vehicleDescription)")
-    print("vehicleManufacturerName     : \(self.vehicleManufacturerName)")
-    print("insuranceProviderName       : \(self.insuranceProviderName)")
-    print("noOfSeat                    : \(self.noOfSeat)")
-    print("fuelType                    : \(self.fuelType)")
-    print("baseRate                    : \(self.baseRate)")
-    print("ratePerKm                   : \(self.ratePerKm)")
-    print("vehicleType                 : \(self.vehicleType)")
-    
-}
 }
 
