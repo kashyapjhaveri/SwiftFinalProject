9bababa1f1f1405f186d6b6c51c05e7b9554bf86
shreemarella <mvsrikanth96@gmail.com>
2020-02-21 12:31:05 -0500

    [Shree Marella] modified bus class after changing vehicle to protocol

--- SwiftFinalProject/Vehicle/Bus.swift
+++ SwiftFinalProject/Vehicle/Bus.swift
@@ -10,22 +10,53 @@ import Foundation
 
 class Bus : Vehicle
 {
-    var typeOfBus : String
-    var isAccessibilityAvailable : Bool
-    var isWifiAvailable : Bool
+    var vehicleIdentificationNumber : String
+    var vehicleDescription          : String
+    var vehicleManufacturerName     : String
+    var isSelfDrive                 : Bool
+    var isInsured                   : Bool
+    var insuranceProviderName       : String
+    var noOfSeat                    : Int
+    var fuelType                    : Fuel
+    var baseRate                    : Int
+    var ratePerKm                   : RatePerKm
+    var vehicleType                 : String
+    var typeOfBus                   : String
+    var isAccessibilityAvailable    : Bool
+    var isWifiAvailable             : Bool
     
-    init(_ vehicleIdentificationNumber: String,_ vehicleDescription: String, _ vehicleManufacturerName: String,_ isSelfDrive: Bool,_ isInsured: Bool, _ insuranceProviderName: String,_ noOfSeat: Int,_ fuelType: Fuel,_ baseRate: Int, _ ratePerKm: Int,_ vehicleType: String,_ typeOfBus:String, _ isAccessibilityAvailable:Bool,_ isWifiAvailable:Bool)
+    init(vehicleIdentificationNumber:String,vehicleDescription:String,vehicleManufacturerName:String,isSelfDrive:Bool,isInsured:Bool,insuranceProviderName:String,noOfSeat:Int,fuelType:Fuel,baseRate:Int,ratePerKm:RatePerKm,vehicleType:String,typeOfBus:String,isAccessibilityAvailable:Bool,isWifiAvailable:Bool)
     {
-        
+        self.vehicleIdentificationNumber = vehicleIdentificationNumber
+        self.vehicleDescription = vehicleDescription
+        self.vehicleManufacturerName = vehicleManufacturerName
+        self.isSelfDrive = isSelfDrive
+        self.isInsured = isInsured
+        self.insuranceProviderName = insuranceProviderName
+        self.noOfSeat = noOfSeat
+        self.fuelType = Fuel
+        self.baseRate = baseRate
+        self.ratePerKm = ratePerKm
+        self.vehicleType = vehicleType
         self.typeOfBus = typeOfBus
         self.isAccessibilityAvailable = isAccessibilityAvailable
         self.isWifiAvailable = isWifiAvailable
-        super.init(vehicleIdentificationNumber,vehicleDescription,vehicleManufacturerName,isSelfDrive,isInsured,insuranceProviderName,noOfSeat,fuelType,baseRate,ratePerKm,vehicleType)
     }
-    override func Display() {
-        super.Display()
-        print("typeOfBus                   : \(self.typeOfBus)")
-        print("isAccessibilityAvailable    : \(self.isAccessibilityAvailable)")
-        print("isWifiAvailable             : \(self.isWifiAvailable)")
+    
+    func Display()
+    {
+        print("Vehicle Identification Number : \(self.vehicleIdentificationNumber)")
+        print("Vehicle Description           : \(self.vehicleDescription)")
+        print("Vehicle Manufacturer Name     : \(self.vehicleManufacturerName)")
+        print("InsuranceProviderName         : \(self.insuranceProviderName)")
+        print("No Of Seat                    : \(self.noOfSeat)")
+        print("Fuel Type                     : \(self.Fuel)")
+        print("Base Rate                     : \(self.baseRate)")
+        print("Rate Per Km                   : \(self.ratePerKm)")
+        print("Vehicle Type                  : \(self.vehicleType)")
+        print("Type Of Bus                   : \(self.typeOfBus)")
+        print("Is Accessibility Available    : \(self.isAccessibilityAvailable)")
+        print("Is WIFI Available             : \(self.isWifiAvailable)")
     }
+    
 }
