c986b864702bd0e11cb83d450c713c5977120569
Kashyap Jhaveri <jhaverikashyap1998@gmail.com>
2020-02-22 15:22:45 -0500

    [Kashyap Jhaveri] Added method to init all customers object and store in dictionary

--- SwiftFinalProject/main.swift
+++ SwiftFinalProject/main.swift
@@ -8,50 +8,68 @@
 
 import Foundation
 
-print("Hello, World!")
-
 
 //var temp = Bus(vehicleIdentificationNumber: "gsrg", vehicleDescription: "hvbv", vehicleManufacturerName: "bbbs", isSelfDrive: true, isInsured: false, insuranceProviderName: "dvs", noOfSeat: 2, fuelType: Fuel.PETROL, baseRate: BaseRate.BUS, ratePerKm: RatePerKm.BUS, vehicleType: "Bua", typeOfBus: "Coch", isAccessibilityAvailable: true, isWifiAvailable: false);
 //
 //temp.Display();
 
-var BusesDT = [String : Bus]()
-for b in BusesDT
-{
-    b.value.Display()
-    print(" \t ********Bus Details********")
+//var BusesDT = [Int : Bus]()
+//var CarsDT = [Int : Car]()
+//var MotorcyclesDT = [Int : MotorCycle]()
+var ownersDT = [Int : Owner]()
+//var DriversDT = [Int: Driver]()
+var CustomersDT = [Int: Customer]()
+
+
+func readCustomersToDictonary()  {
+    let readFileInstance = ReadFile.getInsatnce();
+    
+    do {
+        let json = try readFileInstance.readFileToJSON(fileName: "Customers", ext: "json")
+
+        if let customers = json as? [Any]{
+            
+            for customer in customers {
+                
+                var tempGender:Gender;
+                let cust = customer as! [String:Any];
+                
+                if cust["gender"] as! String == "Male"{
+                    tempGender = Gender.Male;
+                }
+                else if cust["gender"] as! String == "Female"
+                {
+                    tempGender = Gender.Female;
+                }
+                else{
+                    tempGender = Gender.Others;
+                }
+                
+                let tempDate = Date.from(date: cust["birthDate"] as! String)
+                
+                let tempObj = Customer(id: cust["id"] as! Int, firstname: cust["firstName"] as! String, lastname: cust["lastName"] as! String, gender: tempGender, birthDate:tempDate!, mobileNumber: cust["mobileNumber"] as! String, email: cust["email"] as! String, username: cust["userName"] as! String, password: cust["password"] as! String, address: cust["address"] as! String, city: cust["city"] as! String)
+                
+                CustomersDT.updateValue(tempObj, forKey: cust["id"] as! Int);
+            }
+        }
+    }
+    catch FileExceptions.fileNotFound{
+        print("File not found")
+    }
+    catch FileExceptions.dataReadingError {
+        print("Error while reading data from file");
+    }
+    catch FileExceptions.errorPasingToJSON{
+        print("Error while parsing data to json")
+    }
+    catch {
+        print("Invalid Error")
+    }
 }
 
-var CarsDT = [String : Car]()
-for c in CarsDT
-{
-    c.value.Display()
-    print(" \t ********Car Details********")
+func initAllValues() {
+  readCustomersToDictonary()
 }
 
-var MotorcyclesDT = [String : MotorCycle]()
-for m in MotorcyclesDT
-{
-    m.value.Display()
-    print(" \t *******MotorCycles Details********")
-}
+initAllValues()
 
-var ownerDT = [String : Owner]()
-for x in ownerDT
-{
-    x.value.Display()
-    print("\t *********owner details *********")
-}
-
-var DriverDT = [String: Driver]()
-for y in DriverDT
-{
-    y.value.Display()
-    print("\t********Driver details**************")
-}
-var CustomerDT = [String: customer]()
-for z in CustomerDT
-{
-    z.value.Display()
-    print("\t**********Customer details**********")
-}
