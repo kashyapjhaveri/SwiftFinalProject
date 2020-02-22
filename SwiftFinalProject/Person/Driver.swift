720377fba747773c116f414754c2d5e108dee811
gagandeepkaur <sherry2312.gk@gmail.com>
2020-02-22 01:37:02 -0500

    [gk] driver class  modified

--- SwiftFinalProject/Person/Driver.swift
+++ SwiftFinalProject/Person/Driver.swift
@@ -10,24 +10,53 @@ import Foundation
 
 class Driver : Person
 {
+    var id                    : Int
+    var firstname             : String
+    var lastname              : String
+    var gender                : Gender
+    var birthDate             : Date
+    var age                   : Int
+    var mobileNumber          : String
+    var email                 : String
+    var username              : String
+    var password              : String
     var  drivingLicenceNumber : String
-    var isHistoryCleared : Bool
-    var salary : Double
+    var isHistoryCleared      : Bool
+    var salary                : Double
     
     
-    init (id: String, firstname: String, lastname: String, gender: Gender, birthDate: Date, age: Int, mobileNumber: String, email: String, username: String, password: String , drivingLicenceNumber : String ,isHistoryCleared : Bool , salary : Double )
+    init (id: Int, firstname: String, lastname: String, gender: Gender, birthDate: Date, age: Int, mobileNumber: String, email: String, username: String, password: String , drivingLicenceNumber : String ,isHistoryCleared : Bool , salary : Double )
     {
+        self.id                   = id
+        self.firstname            = firstname
+        self.lastname             = lastname
+        self.gender               = gender
+        self.birthDate            = birthDate
+        self.age                  = birthDate.getAge()
+        self.mobileNumber         = mobileNumber
+        self.email                = email
+        self.username             = username
+        self.password             = password
         self.drivingLicenceNumber = drivingLicenceNumber
-        self.isHistoryCleared = isHistoryCleared
-        self.salary = salary
-        super.init(id , firstname, lastname, gender, birthDate, age, mobileNumber, email, username , password)
+        self.isHistoryCleared     = isHistoryCleared
+        self.salary               = salary
+        
     }
     
-    override  func Display()
+     func Display()
     {
-        super.Display()
-        print("drivingLicenceNumber        :     \(self.drivingLicenceNumber)")
-        print("isHistoryCleared            :     \(self.isHistoryCleared)")
-        print("salary                      :     \(self.salary)")
+        print (" id                        : \(self.id)")
+        print (" firstname                 : \(self.firstname)")
+        print (" lastname                  : \(self.lastname)")
+        print (" gender                    : \(self.gender)")
+        print (" birthDate                 : \(self.birthDate)")
+        print (" age                       : \(self.age)")
+        print (" mobileNumber              : \(self.mobileNumber)")
+        print (" email                     : \(self.email)")
+        print (" username                  : \(self.username)")
+        print (" password                  : \(self.password)")
+        print("drivingLicenceNumber        : \(self.drivingLicenceNumber)")
+        print("isHistoryCleared            : \(self.isHistoryCleared)")
+        print("salary                      : \(self.salary)")
     }
 }
