2723175fb7ddd3d3ad9d7a67dbe3b71005721092
Kashyap Jhaveri <jhaverikashyap1998@gmail.com>
2020-02-22 15:11:12 -0500

    [Kashyap Jhaveri] Added Custom exception for file

---
+++ SwiftFinalProject/Exceptions/exceptions.swift
@@ -0,0 +1,15 @@
+//
+//  exceptions.swift
+//  SwiftFinalProject
+//
+//  Created by Kashyap Jhaveri on 2020-02-22.
+//  Copyright © 2020 Kashyap Jhaveri. All rights reserved.
+//
+
+import Foundation
+
+enum FileExceptions : Error{
+    case fileNotFound
+    case dataReadingError
+    case errorPasingToJSON
+}
